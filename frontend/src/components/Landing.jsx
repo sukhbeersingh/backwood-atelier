ac5b8a964e699053533636bc18a1439b060e38f8
Sukhbeeee <sukhbeeee@mac.home>
2025-05-03 22:59:56 -0400

    add landing page

---
+++ frontend/src/components/Landing.jsx
@@ -0,0 +1,14 @@
+import React from 'react'; // Import CSS for styling
+import './Landing.css'; // Import the CSS file for styling
+const LandingPage = () => {
+    return (
+        <div className="landing-page">
+            <div className="content">
+                <h1 className="roboto-h1">Where Craftsmanship Meets Nature </h1>
+                {/* <h2 className="roboto-h2">In The Heart Of A Quaint Small Town Nestled At The Serene Foothills Of Kumaon</h2> */}
+            </div>
+        </div>
+    );
+};
+
+export default LandingPage;
